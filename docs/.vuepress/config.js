c45d185f56626c248306b9b94ac6a2707deba3a6
lmk123 <milk.lee@qq.com>
2020-04-08 23:53:00 +0800

    build: 添加谷歌分析插件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,15 @@
 module.exports = {
   title: '划词翻译',
   description: '让浏览不同语言的网站变得无比轻松的浏览器扩展程序。',
-  plugins: ['@vuepress/nprogress', '@vuepress/back-to-top','@vuepress/active-header-links']
+  plugins: [
+    '@vuepress/nprogress',
+    '@vuepress/back-to-top',
+    '@vuepress/active-header-links',
+    [
+      '@vuepress/google-analytics',
+      {
+        ga: 'UA-163159722-1',
+      },
+    ],
+  ],
 }
